5c30883378245cc0e00743739d0dcfb0542766aa
EduHoff <ehcs.business@gmail.com>
2026-05-27 11:02:45 -0300

    Contagem Regressiva

---
+++ src/exercises/Ex5.java
@@ -0,0 +1,39 @@
+package exercises;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import utils.ConsoleUtils;
+import utils.Recursion;
+
+public class Ex5 implements Exercise {
+
+    @Override
+    public void run(Scanner sc) {
+        System.out.println("--- Exercicío 5 ---");
+
+        int num = 0;
+        while (true) {
+            try{
+                System.out.println("Digite um número para começar a contagem regressiva:");
+                System.out.print("||");
+                num = sc.nextInt();
+                break;
+            }catch(InputMismatchException e){
+                ConsoleUtils.clear();
+                System.out.println("Digite apenas números inteiros positivos!\n");
+                sc.nextLine();
+            }catch(IndexOutOfBoundsException e){
+                ConsoleUtils.clear();
+                System.out.println("A posição precisa ser maior que zero!\n");
+                sc.nextLine();
+            }catch(Exception e){
+                ConsoleUtils.clear();
+                e.printStackTrace();
+            } 
+        }
+
+        ConsoleUtils.clear();
+        Recursion.contagem_regressiva(num);
+    }
+}
--- src/utils/Recursion.java
+++ src/utils/Recursion.java
@@ -24,26 +24,36 @@ public class Recursion {
 
     public static long fibonacci(int pos) {
         if (pos <= 0) throw new IndexOutOfBoundsException("Posição na sequência não pode ser menor ou igual a zero!");
-        return fibonacciTail(pos, 1, 0, 1);
+        return fibonacci_tail(pos, 1, 0, 1);
     }
 
-    private static long fibonacciTail(int pos, int acc, int penult_num, int ult_num) {
+    private static long fibonacci_tail(int pos, int acc, int penult_num, int ult_num) {
         if (acc == pos)
             return ult_num;
 
-        return fibonacciTail(pos, acc + 1, ult_num, penult_num + ult_num);
+        return fibonacci_tail(pos, acc + 1, ult_num, penult_num + ult_num);
     }
 
     public static int potencia(int x, int y){
         if(y < 0) throw new InputMismatchException("Y não pode ser negativo!");
         if(y == 0 && x == 0) throw new ArithmeticException("Impossível elevar 0 por 0");
-        return potenciaTail(x, y, 1);
+        return potencia_tail(x, y, 1);
     }
 
-    private static int potenciaTail(int x, int y, int acc){
+    private static int potencia_tail(int x, int y, int acc){
         
         if(y == 0) return acc;
         
-        return potenciaTail(x, y-1, acc * x);
+        return potencia_tail(x, y-1, acc * x);
     }
+
+    public static void contagem_regressiva(int num){
+        if(num < 0) throw new InputMismatchException("Número não pode ser negativo!");
+
+        System.out.println(num + "!");
+
+        if(num == 0) return;
+        contagem_regressiva(num-1);
+    }
+
 }
