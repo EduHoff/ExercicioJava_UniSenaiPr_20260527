a83f7a24dc32663314040f58cac1d2de4f8af8f4
EduHoff <ehcs.business@gmail.com>
2026-05-27 10:28:25 -0300

    Fibonacci

--- src/exercises/Ex3.java
+++ src/exercises/Ex3.java
@@ -6,22 +6,26 @@ import java.util.Scanner;
 import utils.ConsoleUtils;
 import utils.Recursion;
 
-public class Ex2 implements Exercise {
+public class Ex3 implements Exercise {
 
     @Override
     public void run(Scanner sc) {
-        System.out.println("--- Exercicío 2 ---");
+        System.out.println("--- Exercicío 3 ---");
 
-        int num = 0;
+        int pos = 1;
         while (true) {
             try{
-                System.out.println("Digite um número para calcular a soma dos n primeiros números naturais:");
+                System.out.println("Digite qual posição da sequência de Fibonacci quer calcular o número:");
                 System.out.print("||");
-                num = sc.nextInt();
+                pos = sc.nextInt();
                 break;
             }catch(InputMismatchException e){
                 ConsoleUtils.clear();
                 System.out.println("Digite apenas números inteiros positivos!\n");
+                sc.nextLine();
+            }catch(IndexOutOfBoundsException e){
+                ConsoleUtils.clear();
+                System.out.println("A posição precisa ser maior que zero!\n");
                 sc.nextLine();
             }catch(Exception e){
                 ConsoleUtils.clear();
@@ -30,7 +34,7 @@ public class Ex2 implements Exercise {
         }
 
         ConsoleUtils.clear();
-        long result = Recursion.soma_numeros_naturais(num);
-        System.out.println("O somatório resultou em " + result);
+        long result = Recursion.fibonacci(pos);
+        System.out.println(result + " é o " + pos + "º número da sequência de Fibonacci!");
     }
 }
--- src/utils/Recursion.java
+++ src/utils/Recursion.java
@@ -22,4 +22,15 @@ public class Recursion {
             return num + soma_numeros_naturais(num-1);
     }
 
+    public static long fibonacci(int pos) {
+        if (pos <= 0) throw new IndexOutOfBoundsException("Posição na sequência não pode ser menor ou igual a zero!");
+        return fibonacciTail(pos, 1, 0, 1);
+    }
+
+    private static long fibonacciTail(int pos, int acc, int penult_num, int ult_num) {
+        if (acc == pos)
+            return ult_num;
+
+        return fibonacciTail(pos, acc + 1, ult_num, penult_num + ult_num);
+    }
 }
